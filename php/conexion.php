f4e8c5c8e90ddafb57f395799daae94ee910db6c
Isauro Rivera <isauro.rivera@unl.edu.ec>
2024-08-01 10:17:37 -0500

    login

---
+++ php/conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+
+    $conexion = mysqli_connect("localhost","root","","login_register2_db");
+    /*/
+    if($conexion){
+       echo 'Conectado exitosamente a la base de datos';
+    }else{
+        echo 'NO se ha podido conectar a las base de datos';
+    }
+    */    
+
+    
+
+?>
